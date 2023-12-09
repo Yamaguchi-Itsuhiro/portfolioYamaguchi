7ba6694634f553c39e3cd7232261eecdf1d4d87b
Yamaguchi-Itsuhiro <152586889+Yamaguchi-Itsuhiro@users.noreply.github.com>
2023-12-09 16:06:52 +0900

    Add files via upload

---
+++ portfolio/trainning/htmlTrainning.js
@@ -0,0 +1,9 @@
+document. getElementById("text") .onclick = function(){
+    document. getElementById("text"). innerHTML= "クリックされた";
+};
+
+const AAA = document.getElementById("BTN")
+
+AAA.addEventListener("click",function(){
+    prompt("名前を入力してください")
+})
