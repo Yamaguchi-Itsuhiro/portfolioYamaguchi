ccdc96be8911f17171e53318dd437dd249ed2f47
Yamaguchi-Itsuhiro <152586889+Yamaguchi-Itsuhiro@users.noreply.github.com>
2023-12-09 16:10:48 +0900

    Delete portfolio directory

--- portfolio/trainning/htmlTrainning.js
+++
@@ -1,9 +0,0 @@
-document. getElementById("text") .onclick = function(){
-    document. getElementById("text"). innerHTML= "クリックされた";
-};
-
-const AAA = document.getElementById("BTN")
-
-AAA.addEventListener("click",function(){
-    prompt("名前を入力してください")
-})
